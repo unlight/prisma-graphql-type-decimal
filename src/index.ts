78faa44f338532f25eb87d74a894b9698413e04a
Roman_Vasilev <s91630277@gmail.com>
2021-04-21 01:18:27 +0400

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,34 @@
-import { PlainObject } from 'simplytyped';
+import { Decimal } from 'decimal.js';
+import { GraphQLScalarType, GraphQLScalarTypeConfig, Kind } from 'graphql';
 
-const dict: PlainObject = {};
+const config: GraphQLScalarTypeConfig<null | Decimal, null | string> = {
+    name: 'Decimal',
+    description: 'An arbitrary-precision Decimal type',
+    /**
+     * Value sent to the client
+     */
+    serialize(value: Decimal) {
+        // console.log('serialize', value, value.constructor.name);
+        return value.toString();
+    },
+    /**
+     * Value from the client
+     */
+    parseValue(value: string) {
+        console.log('parseValue', value, value.constructor.name);
+        return value;
+    },
+    parseLiteral(ast) {
+        if (
+            ast.kind === Kind.INT ||
+            ast.kind === Kind.FLOAT ||
+            ast.kind === Kind.STRING
+        ) {
+            return new Decimal(ast.value);
+        }
+        // eslint-disable-next-line unicorn/no-null
+        return null;
+    },
+};
 
-/**
- * Hello function whithout parameter
- * @returns result string
- */
-export function hello(): string;
-
-/**
- * This is hello function
- * @returns result string
- */
-export function hello(greet = 'Hello') {
-    return `${greet} world`;
-}
-
-export class X {
-    constructor(private readonly o: PlainObject) {}
-}
+export const GraphQLDecimal = new GraphQLScalarType(config);
